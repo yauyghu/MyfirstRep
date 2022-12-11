c17f5f53caa46040a97f99e8a4519df5d5f944a4
Оксана <nizhin.www@gmail.com>
2022-12-11 22:52:52 +0200

    init1

---
+++ src/dz3/Workssss.java
@@ -0,0 +1,12 @@
+package dz3;
+
+public class Workssss {
+    public static void main(String[] args) {
+        Worker = new Workers("Yaroslav Evdokymenko", "junior",
+                "nizhin.www@gmail.com", "0984247997", 20);
+        System.out.println(Workers.getName() + ", phone:" + Workers.getPhone() + "\n--------------------------");
+
+        Car car = new Car();
+        car.start();
+    }
+}
